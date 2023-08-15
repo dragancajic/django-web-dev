c157089adbbafa58c8794fb34b27ca0afceebdc6
Драган Ћајић <cajic_dragan@yahoo.com>
2023-08-15 03:25:02 +0200

    ~ Web Dev Task 05-02: `Fix Sending Data To Your Template`
    > NOTE: use populated database from Web Dev Task 05-01!

--- 05_blog_overview/02_fix_sending_data_to_your_template/learning_logs/views.py
+++ 05_blog_overview/02_fix_sending_data_to_your_template/learning_logs/views.py
@@ -5,12 +5,19 @@ from .models import Topic, Entry
 # Create your views here.
 def index(request):
     """The home page for learning logs."""
-    return render(request, 'learning_logs/index.html')
+    topics = Topic.objects.all()
+    print(Topic)
+
+    # create context dictionary to fill with query set object values
+    # context_dict = {'key': query_set_object}
+    context = {'topics': topics}
+
+    return render(request, 'learning_logs/index.html', context)
 
 def topics(request):
     """Displays a list of all available topics."""
     topics = Topic.objects.order_by('-date_added')
-    return render(request, 'learning_logs/topics.html')
+    return render(request, 'learning_logs/topics.html', {'topics': topics})
 
 def entry(request, pk):
     """Displays the full text of a learning entry."""
